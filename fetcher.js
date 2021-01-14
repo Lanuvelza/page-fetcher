035cb296b05de617d8bfaf2afbd2cc52c0db445e
Lanuvelza <ianlam1221@hotmail.com>
2021-01-14 05:47:26 +0000

    updated to include an option to overwritea file

--- fetcher.js
+++ fetcher.js
@@ -1,39 +1,53 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
 const args = process.argv.splice(2);
 
 const URL = args[0];
-const localPath = args[1]; 
+const localPath = args[1];
 
 
 
 const fletcher = function(arg, path) {
   request(arg , (error, response, body) => {
-    if (error || response.statusCode < 200 || response.statusCode > 299) {
-      throw new Error("URL is a non-200 result"); 
+    if (error) {
+      throw new Error(`Error! URL is a non-200 result`);
     }
 
-    fs.access(path, fs.constant.F_OK, (err) => {
-      if (err) {
+    fs.exists(path, (exists) => {
+      if (!exists) {
         throw new Error("file path does not exist");
-      }
-    });
-    // console.log('error:', error); 
-    // console.log('statusCode:', response && response.statusCode); 
-    fs.writeFile(path, body, (err) => {
-      if (err) { 
-        throw err; 
-      } 
+      } else {
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+        });
 
-      fs.stat(path, (err, stats) => {
-        if (err) {
-          throw err;
-        }
-        console.log(`Downdloaded and saved ${stats.size} bytes to ${path}`);
-      });
+        process.stdin.setRawMode(true);
+        process.stdin.setEncoding('utf8');
+
+        rl.question("Do you want to overwrite the file? press 'Y' to overwrite: ", (answer) => {
+          if (answer === "Y") {
+            fs.writeFile(path, body, (err) => {
+              if (err) {
+                throw err;
+              }
+              console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+            });
+          } else {
+            console.log("goodbye");
+            
+          }
+          rl.close();
+          
+        });
+      }
+
     });
+        
   });
 };
+
 
 fletcher(URL,localPath);
